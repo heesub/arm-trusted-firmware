1923e7b142f8bedbed8b62f290c081654d433b94
Lionel Debieve <lionel.debieve@foss.st.com>
2023-04-07 14:42:13 +0200

    fix(st): fix the ssp_otp nvmem for stm32mp13
    STM32MP13 use the cfg9_otp instead of ssp_otp in the
    associated device tree.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Change-Id: I5f9861422209c60931b78574045e62999c9f66d7
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/299660
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- plat/st/stm32mp1/stm32mp1_def.h
+++ plat/st/stm32mp1/stm32mp1_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2022, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2023, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -380,9 +380,11 @@ enum ddr_type {
 #if STM32MP13
 #define NAND_OTP			"cfg9_otp"
 #define NAND2_OTP			"cfg10_otp"
+#define SSP_OTP				"cfg9_otp"
 #endif
 #if STM32MP15
 #define NAND_OTP			"nand_otp"
+#define SSP_OTP				"ssp_otp"
 #endif
 #define MONOTONIC_OTP			"monotonic_otp"
 #define UID_OTP				"uid_otp"
@@ -390,7 +392,6 @@ enum ddr_type {
 #define ENCKEY_OTP			"enckey_otp"
 #define BOARD_ID_OTP			"board_id"
 #define CFG2_OTP			"cfg2_otp"
-#define SSP_OTP				"ssp_otp"
 #define CHIP_CERTIFICATE_OTP		"chip_otp"
 #define RMA_OTP				"rma_otp"
 
